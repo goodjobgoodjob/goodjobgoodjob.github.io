f5d651ea9e1fd77f4df0a86eeba102694d90baa8
goodjobgoodjob <124995423+goodjobgoodjob@users.noreply.github.com>
2023-02-10 16:13:55 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+window.config  = [
+    // window.config = [
+      {
+        url: 'https://wa.me/600148725057'
+      },
+      {
+       
+        url: 'https://wa.me/600148725057'
+      },
+      
+      
+    
+  ];

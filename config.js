863e8fa97a2cac96bd161944719d9bf8f2252b50
goodjobgoodjob <124995423+goodjobgoodjob@users.noreply.github.com>
2023-02-13 21:53:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window.config  = [
     // window.config = [
       {
-        url: 'https://wa.me/85266453089'
+        url: 'https://wa.me/60194242348'
       },
       {
        
-        url: 'https://wa.me/85266453089'
+        url: 'https://wa.me/60194242348'
       },
       
       
